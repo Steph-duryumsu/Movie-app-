d3d08c861cca788655d6d3e081ce10b465b32f74
Steph-duryumsu <yetbudam@gmail.com>
2025-05-23 03:50:30 +0100

    updated login and signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,22 +18,31 @@ const Login = () => {
     <div className="h-screen flex flex-col justify-center items-center animate-gradient bg-gradient-to-r from-purple-300 to-blue-400">
       <form onSubmit={handleSubmit} className="bg-white p-8 rounded shadow-md w-80">
         <h2 className="text-xl font-semibold mb-4 text-center">Login</h2>
+
+        <label className="text-sm text-gray-600">Email</label>
+        <div className="relative mb-3">
         <input
           type="email"
-          placeholder="Email"
-          className="w-full mb-3 p-2 border rounded"
+          placeholder="Enter you email"
+          className="w-full mb-3 p-2 pl-10 border rounded"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
-        />
+        /><i class="fa-regular fa-envelope absolute left-3 top-5 left-3 text-gray-400 transform -translate-y-1/2 "></i>
+        </div>
+
+        <label className="text-sm text-gray-600">Password</label>
+        <div className="relative mb-3">
         <input
           type="password"
-          placeholder="Password"
-          className="w-full mb-3 p-2 border rounded"
+          placeholder="Enter your password"
+          className="w-full mb-3 p-2 pl-10 border rounded"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
         />
+        <i class="fa-regular fa-eye absolute left-3 top-5 left-2 text-gray-400 transform -translate-y-1/2"></i>
+        </div>
         <button
           type="submit"
           className="w-full bg-purple-600 hover:bg-purple-00 text-white py-2 rounded "
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -17,32 +17,49 @@ const SignUp = () => {
 
   return (
     <div className="h-screen flex flex-col justify-center items-center animate-gradient bg-gradient-to-r from-purple-300 to-blue-400">
-      <form onSubmit={handleSubmit} className="bg-white p-8 rounded shadow-md w-80">
+      <div className="">
+      <form onSubmit={handleSubmit} className="bg-white p-8 rounded shadow-md w-100">
         <h2 className="text-xl font-semibold mb-4 text-center">Sign Up</h2>
+
+        <label className="text-sm text-gray-600">Username</label>
+        <div className="relative mb-3">
         <input
           type="text"
-          placeholder="Username"
-          className="w-full mb-3 p-2 border rounded"
+          placeholder="Enter your username"
+          className="w-full mb-3 p-2 pl-10 border rounded"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           required
         />
+        <i class="fa-regular fa-circle-user absolute left-3 top-5 left-3 text-gray-400 transform -translate-y-1/2"></i>
+        </div>
+
+        <label className="text-sm text-gray-600">Email</label>
+        <div className="relative mb-3">
         <input
           type="email"
-          placeholder="Email"
-          className="w-full mb-3 p-2 border rounded"
+          placeholder="Enter your email"
+          className="w-full mb-3 p-2 pl-10 border rounded"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
+        <i class="fa-regular fa-envelope absolute left-3 top-5 left-3 text-gray-400 transform -translate-y-1/2 "></i>
+        </div>
+
+        <label className="text-sm text-gray-600">Password</label>
+        <div className="relative mb-3">
         <input
           type="password"
-          placeholder="Password"
-          className="w-full mb-3 p-2 border rounded"
+          placeholder="Enter your password"
+          className="w-full mb-3 p-2 pl-10 border rounded"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
         />
+        <i class="fa-regular fa-eye absolute left-3 top-5 left-2 text-gray-400 transform -translate-y-1/2"></i>
+        </div>
+
         <button
           type="submit"
           className="w-full bg-purple-600 hover:bg-purple-700 text-white py-2 rounded hover:bg-blue-700"
@@ -51,6 +68,7 @@ const SignUp = () => {
         </button>
       </form>
     </div>
+  </div>
   );
 };
 
